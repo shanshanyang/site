e1b84cb4690bc307e595587be6c0c0964b4dd47b
Yang, Shanshan <SYang@vscatny.com>
2017-03-09 11:35:48 -0500

    web + karma + aribnb presets

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const app = document.createElement('main');
+const text = document.createTextNode('hello world!');
+
+app.appendChild(text);
+document.getElementById('root').appendChild(app);
---
+++ test/simple_test.js
@@ -0,0 +1,21 @@
+import assert from 'assert';
+import sum from '../src/comp.js';
+
+
+describe('simple', () => {
+  it('should be sane', () => {
+    assert.equal(true, !false);
+  });
+});
+
+describe('sum Acceptance test cases', () => {
+  it('2+3 should return correct sum value', () => {
+    assert.equal(sum(2,3), 5);
+  });
+  it('0+5 should also return correct sum value', () => {
+    assert.equal(sum(0,5), 5);
+  });
+  it('-1+6 should also return correct sum value', () => {
+    assert.equal(sum(-1,6), 5);
+  });
+});
