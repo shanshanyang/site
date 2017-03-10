ca09584516d643e5ec08d3d3f701baec8831d1fa
Yang, Shanshan <SYang@vscatny.com>
2017-03-10 13:27:19 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import sum from './comp';
+
 const app = document.createElement('main');
-const text = document.createTextNode('hello world!');
+const text = document.createTextNode(`hello world! ${sum(2, 3)}`);
 
 app.appendChild(text);
 document.getElementById('root').appendChild(app);
